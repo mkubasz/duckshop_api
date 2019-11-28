27a07a9635346ab1123a1760ab41cdeb5170aad5
mkubasz <mkubasz@gmail.com>
2019-11-28 23:51:30 +0100

    Add enpoints in base route

--- Main.js
+++ Main.js
@@ -3,6 +3,7 @@ const config = require('./Config');
 const BucketsController = require('./bucket/api/BucketsController');
 const CommandBus = require('./application/CommandBus');
 const QueryBus = require('./infrastructure/QueryBus');
+const fs = require('fs');
 
 const InMemoryDataProvider = require('./infrastructure/InMemoryDataProvider')();
 
@@ -12,17 +13,23 @@ const server = restify.createServer({
     url: config.hostname
 });
 
+server.get('/', (req, res, next) => {
+    const endpoints = fs.readFileSync('endpoints.json');
+    res.send(JSON.parse(endpoints));
+    next();
+});
+
 const commandBus = CommandBus(InMemoryDataProvider);
 const queryBus = QueryBus(InMemoryDataProvider);
 
 /// Buckets Endpoints
-server.get('/bucket',async (req, res, next) => {
+server.get('/bucket', async (req, res, next) => {
     const message = await BucketsController({commandBus, queryBus}).getAll();
     res.send(message);
     next();
 });
 
-server.get('/bucket/:id',async (req, res, next) => {
+server.get('/bucket/:id', async (req, res, next) => {
     const message = await BucketsController({commandBus, queryBus}).get(req.params.id);
     res.send(message);
     next();
