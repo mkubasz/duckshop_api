687ac6ae34881b9f42c41ade09a5fc8a31ab4883
mkubasz <mkubasz@gmail.com>
2019-11-29 00:42:49 +0100

    Refactor main and endpoints

--- Main.js
+++ Main.js
@@ -2,30 +2,30 @@ const fastify = require('fastify')({
     logger: true
 });
 
+const fs = require('fs');
 const config = require('./Config');
+
 const BucketsController = require('./bucket/api/BucketsController');
 const CommandBus = require('./application/CommandBus');
 const QueryBus = require('./infrastructure/QueryBus');
-const fs = require('fs');
-
 const InMemoryDataProvider = require('./infrastructure/InMemoryDataProvider')();
+
+const commandBus = CommandBus(InMemoryDataProvider);
+const queryBus = QueryBus(InMemoryDataProvider);
 
 fastify.get('/', (request, reply) => {
     const endpoints = fs.readFileSync('endpoints.json');
     reply.send(JSON.parse(endpoints));
 });
 
-const commandBus = CommandBus(InMemoryDataProvider);
-const queryBus = QueryBus(InMemoryDataProvider);
-
 /// Buckets Endpoints
-fastify.get('/bucket', async (request, reply) => {
+fastify.get('/buckets', async (request, reply) => {
     const message = await BucketsController({commandBus, queryBus}).getAll();
     reply.send(message);
 });
 
-fastify.get('/bucket/:id', async (request, reply) => {
-    const message = await BucketsController({commandBus, queryBus}).get(req.params.id);
+fastify.get('/buckets/:id', async (request, reply) => {
+    const message = await BucketsController({commandBus, queryBus}).get(request.params.id);
     reply.send(message);
 });
 
