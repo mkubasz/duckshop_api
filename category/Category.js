bb539550b61de61fefabb3db8eca2cf68c7e78ed
mkubasz <mkubasz@gmail.com>
2019-12-13 14:44:37 +0100

    Add category

---
+++ category/Category.js
@@ -0,0 +1,7 @@
+const Category = Object.freeze({
+    fruits: 0,
+    meat: 1,
+    other: 2
+});
+
+module.exports = Category;
--- infrastructure/InMemoryDataProvider.js
+++ infrastructure/InMemoryDataProvider.js
@@ -1,11 +1,25 @@
+const Category = require('../category/Category');
+
 const InMemoryDataProvider = () => {
     let buckets = [{
         id: "generatedID1",
         name: "Default1",
         created_at: "data",
-        closed_at: null
+        closed_at: null,
+        position: 0
     }];
-    let product = [];
+    let product = [{
+        id: "generatedID1",
+        bucketID: "generatedID1",
+        name: "Default1",
+        description: "Default1",
+        amount: 1,
+        category: Category.fruits,
+        bought_at: null,
+        image_name: "some.jpg",
+        image: new ArrayBuffer(1),
+        position: 0
+    }];
     return {buckets, product};
 };
 
